f056da610f9f4d283a5f495072a335da8b5894a6
devadigapratham <devadigapratham8@gmail.com>
2024-06-05 10:06:40 +0530

    few errors, will fix later

--- cmd/main.go
+++ cmd/main.go
@@ -4,65 +4,26 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/google/uuid"
-	"go.etcd.io/bbolt"
+	"github.com/devadigapratham/VaDaBase/vadabase"
 )
 
 func main() {
-	// Open the database file
-	db, err := bbolt.Open(".db", 0666, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	// Define user data
 	user := map[string]string{
 		"name": "Prathamesh",
-		"age":  "19",
+		"age":  "20",
 	}
 
-	// Store data in the database
-	err = db.Update(func(tx *bbolt.Tx) error {
-		bucket, err := tx.CreateBucketIfNotExists([]byte("users"))
-		if err != nil {
-			return err
-		}
-
-		id := uuid.New().String()
-		for k, v := range user {
-			if err := bucket.Put([]byte(k), []byte(v)); err != nil {
-				return err
-			}
-		}
-
-		if err := bucket.Put([]byte("id"), []byte(id)); err != nil {
-			return err
-		}
-
-		return nil
-	})
+	_ = user
+	db, err := vadabase.New()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Retrieve data from the database
-	userData := make(map[string]string)
-	err = db.View(func(tx *bbolt.Tx) error {
-		bucket := tx.Bucket([]byte("users"))
-		if bucket == nil {
-			return fmt.Errorf("Bucket (%s) not found!", "users")
-		}
+	db.Insert("users", "name", "")
+	// coll, err := db.CreateCollection("users")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-		err := bucket.ForEach(func(k, v []byte) error {
-			userData[string(k)] = string(v)
-			return nil
-		})
-		return err
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(user)
+	fmt.Printf("%+v\n", coll)
 }
--- vadabase/vadabase.go
+++ vadabase/vadabase.go
@@ -3,12 +3,19 @@ package vadabase
 import (
 	"fmt"
 
+	"github.com/google/uuid"
 	"go.etcd.io/bbolt"
 )
 
 const (
 	defaultDBName = "default"
 )
+
+type M map[string]string
+
+type Collection struct {
+	*bbolt.Bucket
+}
 
 type Vadabase struct {
 	db *bbolt.DB
@@ -24,3 +31,60 @@ func New() (*Vadabase, error) {
 		db: db,
 	}, nil
 }
+
+func (h *Vadabase) CreateCollection(name string) (*Collection, error) {
+	coll := Collection{}
+	err := h.db.Update(func(tx *bbolt.Tx) error {
+		var (
+			err    error
+			bucket *bbolt.Bucket
+		)
+		bucket = tx.Bucket([]byte(name))
+		if bucket == nil {
+			bucket, err := tx.CreateBucket([]byte(name))
+			if err != nil {
+				return err
+			}
+		}
+		coll.Bucket = bucket
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	return &coll, nil
+}
+
+func (h *Vadabase) Insert(collName string, data M) (uuid.UUID, error) {
+	id := uuid.New()
+	coll, err := h.CreateCollection(collName)
+	if err != nil {
+		return id, err
+	}
+	for k, v := range data {
+		if err := coll.Put([]byte(k), []byte(v)); err != nil {
+			return id, err
+		}
+	}
+	if err := coll.Put([]byte("id"), []byte(id.String())); err != nil {
+		return id, err
+	}
+}
+
+func (h *Vadabase) String(coll string, k string, query any) {}
+
+// db.Update(func(tx *bbolt.Tx) error {
+// 	id := uuid.New().String()
+// 	for k, v := range user {
+// 		if err := bucket.Put([]byte(k), []byte(v)); err != nil {
+// 			return err
+// 		}
+// 	}
+
+// 	if err := bucket.Put([]byte("id"), []byte(id)); err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// })
